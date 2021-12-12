6dddda8a1ec8e98cccab3c2303e30a4dbf39461f
allenalvin333 <allenalvin333@gmail.com>
2021-12-12 17:04:34 +0530

    Send a Mail

--- 3.py
+++ 3.py
@@ -1,12 +1,17 @@
+import os
+import ssl
 import smtplib
-
-s = 'YOUR_EMAIL@EMAIL.COM'               
-p = 'YOUR_PASSWORD'                   
+from dotenv import load_dotenv
+load_dotenv() 
+s = os.getenv('username')              
+p = os.getenv('password')             
 
 def email(r, subject, body):
+    context = ssl.create_default_context()
     message = f'Subject: {subject}\n\n{body}'
     with smtplib.SMTP_SSL('smtp.gmail.com', 465) as server:
         server.login(s, p)
         server.sendmail(s, r, message)
+    server.quit()
 
-email('allenbphilip@gmail.com', 'Notification', 'Everything is awesome!')
+email('allenbphilip@gmail.com', 'Python', 'Everything is awesome!')
