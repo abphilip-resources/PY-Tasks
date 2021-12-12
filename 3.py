51366619294cbc62f8330122d116d18cb7a90c05
allenalvin333 <allenalvin333@gmail.com>
2021-12-12 14:34:29 +0530

    programs

--- 3.py
+++ 3.py
@@ -1,13 +1,12 @@
 import smtplib
 
-SENDER_EMAIL = 'YOUR_EMAIL@EMAIL.COM'               # replace with your email address
-SENDER_PASSWORD = 'YOUR_PASSWORD'                   # replace with your email password
+s = 'YOUR_EMAIL@EMAIL.COM'               
+p = 'YOUR_PASSWORD'                   
 
-def send_email(receiver_email, subject, body):
-    message = 'Subject: {}\n\n{}'.format(subject, body)
+def email(r, subject, body):
+    message = f'Subject: {subject}\n\n{body}'
     with smtplib.SMTP_SSL('smtp.gmail.com', 465) as server:
-        server.login(SENDER_EMAIL, SENDER_PASSWORD)
-        server.sendmail(SENDER_EMAIL, receiver_email, message)
+        server.login(s, p)
+        server.sendmail(s, r, message)
 
-if __name__ == '__main__':
-    send_email('RECEIVER@EMAIL.COM', 'Notification', 'Everything is awesome!')
+email('allenbphilip@gmail.com', 'Notification', 'Everything is awesome!')
